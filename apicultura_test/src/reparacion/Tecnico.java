c05b72a8fd424876e9782b86e60ab9be381fd5eb
ArturoChV10 <135257471+ArturoChV10@users.noreply.github.com>
2023-10-05 23:59:38 -0600

    Add files via upload

---
+++ apicultura_test/src/reparacion/Tecnico.java
@@ -0,0 +1,24 @@
+package reparacion;
+
+public class Tecnico {
+
+	private boolean estadoTecnico;
+	private int cantidadTecnicos;
+	
+	public boolean isEstadoTecnico() {
+		return estadoTecnico;
+	}
+	public void setEstadoTecnico(boolean estadoTecnico) {
+		this.estadoTecnico = estadoTecnico;
+	}
+	public int getCantidadTecnicos() {
+		return cantidadTecnicos;
+	}
+	public void setCantidadTecnicos(int cantidadTecnicos) {
+		this.cantidadTecnicos = cantidadTecnicos;
+	}
+	
+	public Tecnico(){
+	
+	}
+}
